6e0b00fdd9b7b90c33182fac50f481d1538eecaf
JIAQI <1010088368@QQ.com>
2020-04-14 11:20:13 +0800

    shiro基础代码整合学习

---
+++ shiro/src/main/java/com/itcast/shiro/realm/UserRealm.java
@@ -0,0 +1,87 @@
+package com.itcast.shiro.realm;
+
+import com.itcast.shiro.constant.UserConstant;
+import com.itcast.shiro.util.SaltUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 功能描述：定义一个自定义的用户授权
+ *
+ * @author JIAQI
+ * @date 2020/4/13 - 11:02
+ */
+@Slf4j
+public class UserRealm extends AuthorizingRealm {
+
+    /* 授权 */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+
+        // 获取用户名
+        String username = (String) principalCollection.getPrimaryPrincipal();
+        log.info("[shiro授权] username:{}", username);
+        // 获取角色
+        Set<String> roles = new HashSet<>();
+        roles.add("admin");
+        // 获取权限
+        Set<String> permissions = new HashSet<>();
+        permissions.add("sys:add");
+        // 将相应的set数据添加到授权信息中
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        info.setRoles(roles);
+        info.setStringPermissions(permissions);
+        return info;
+    }
+
+    /* 认证 */
+    // SimpleCredentialsMatcher ->doCredentialsMatch()
+    // -> MessageDigest -> isEqual
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 验证账号密码
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        log.info("[shiro认证] userName:{}", token.getUsername());
+        log.info("[shiro认证] passWord:{}", token.getPassword());
+
+        // TODO 模拟数据库获取数据
+        // 1.根据用户名获取用户并判断用户是否存在
+        if (!UserConstant.USER.getUserName().equals(token.getUsername())) {
+            throw new UnknownAccountException();
+        }
+        System.out.println(UserConstant.USER.getPassWord());
+        // 将账号密码交给安全管理器进行最后的比对需要
+        // 用户名  密码(这里是指从数据库中获取的password) 当前realm的名称(也可以使用“”替代)
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(token.getUsername(), UserConstant.USER.getPassWord(), getName());
+        // 进行加盐
+        info.setCredentialsSalt(ByteSource.Util.bytes(token.getUsername()));
+        return info;
+    }
+
+    //设置加密方式为MD5
+    /*@Override
+    public void setCredentialsMatcher(CredentialsMatcher credentialsMatcher) {
+        HashedCredentialsMatcher shaCredentialsMatcher = new HashedCredentialsMatcher();
+        shaCredentialsMatcher.setHashAlgorithmName("MD5");
+        shaCredentialsMatcher.setHashIterations(1);
+        shaCredentialsMatcher.setStoredCredentialsHexEncoded(true);
+        super.setCredentialsMatcher(shaCredentialsMatcher);
+    }*/
+
+    public static void main(String[] args) {
+        String oldPassword = new SimpleHash("MD5", "123456",
+                ByteSource.Util.bytes("admin"), 2).toHex();
+        System.out.println(SaltUtil.doSalt("123456", "admin"));
+        System.out.println(oldPassword);
+    }
+
+}
---
+++ shiro/src/main/java/com/itcast/shiro/util/SaltUtil.java
@@ -0,0 +1,23 @@
+package com.itcast.shiro.util;
+
+import org.apache.shiro.crypto.hash.Md5Hash;
+
+/**
+ * 功能描述：MD5盐值加密工具类
+ *
+ * @author JIAQI
+ * @date 2020/4/13 - 14:35
+ */
+public class SaltUtil {
+
+    /**
+     * 进行加盐操作
+     *
+     * @param password 密码
+     * @param salt     盐值
+     * @return 字符串
+     */
+    public static String doSalt(String password, String salt) {
+        return String.valueOf(new Md5Hash(password, salt));
+    }
+}
