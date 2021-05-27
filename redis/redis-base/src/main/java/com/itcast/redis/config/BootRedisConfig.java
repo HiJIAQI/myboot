313bfe73fc3044339688cf70ab22a310dad04ce0
JIAQI <1010088368@QQ.com>
2021-05-27 15:34:06 +0800

    redis cache demo commit

--- redis/redis-base/src/main/java/com/itcast/redis/config/BootRedisConfig.java
+++ redis/redis-base/src/main/java/com/itcast/redis/config/BootRedisConfig.java
@@ -1,14 +1,24 @@
 package com.itcast.redis.config;
 
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 /**
  * 功能描述：redis配置类
@@ -23,7 +33,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @date 2020/4/15 - 15:09
  */
 @Configuration
-public class BootRedisConfig {
+@EnableCaching
+public class BootRedisConfig extends CachingConfigurerSupport {
 
     @Bean("redisTemplate")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
@@ -46,4 +57,58 @@ public class BootRedisConfig {
         template.afterPropertiesSet();
         return template;
     }
+
+    /**
+     * 自定义缓存管理器
+     * 配置目的：配合spring- cache注解 修改其序列化方式
+     * 注意点：如果没有重定义computePrefixWith方法我们可以通过源码分析可知
+     * 在类CacheKeyPrefix中其key构造方式为 == ( name -> name + "::")
+     * 因此在写入redis时会出现两个冒号的问题
+     */
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        // 设置缓存的默认超时时间60分钟
+        redisCacheConfiguration = redisCacheConfiguration.entryTtl(Duration.ofMinutes(5L))
+                // 如果是空值，不缓存
+                .disableCachingNullValues()
+                //覆盖默认的构造key，否则会多出一个冒号
+                .computePrefixWith(name -> name + ":")
+                // 设置key序列化器
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                // 设置value序列化器
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+    }
+
+    /**
+     * redis key生成策略
+     * target: 类
+     * method: 方法
+     * params: 参数
+     *
+     * @return KeyGenerator
+     * 注意: 该方法只是声明了key的生成策略,还未被使用,需在@Cacheable注解中指定keyGenerator
+     * 如: @Cacheable(value = "key", keyGenerator = "cacheKeyGenerator")
+     */
+    @Bean
+    public KeyGenerator cacheKeyGenerator() {
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(method.getName());
+            for (Object obj : params) {
+                // 由于参数可能不同, hashCode肯定不一样, 缓存的key也需要不一样
+                sb.append(JSON.toJSONString(obj).hashCode());
+            }
+            return sb.toString();
+        };
+    }
+
 }
